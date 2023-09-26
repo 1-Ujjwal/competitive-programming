d92e8ee3ce6a51931cd24d67508a17a9babcdd8e
Ujjwal_Kumar <kingayushman7@gmail.com>
2023-09-25 23:44:10 -0800

    Round_899

---
+++ codeforces/codeforces_round_899/A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int m = 1;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == m)
+        {
+            m += 2;
+        }
+        else
+        {
+            m++;
+        }
+    }
+    cout << m - 1 << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
